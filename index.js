7324f79bbafe8c4416f958d411ea72fa026e7a45
Aaron Hemphill <hemphill.aaron@gmail.com>
2023-04-30 11:35:14 +0200

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your solution in this file!
+var customerName = 'bob'
+function upperCaseCustomerName() {
+   return  customerName.toUpperCase();
+}
+
+
+function setBestCustomer(){
+    return bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+   return  bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'me';
+
+function changeLeastFavoriteCustomer(){
+   return leastFavoriteCustomer = 'you';
+}
